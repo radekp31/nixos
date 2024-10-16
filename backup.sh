101574bd29cbecc3ec93cb5530b7e961999eb1ee
Radek Polasek <polasek.31@seznam.cz>
2024-10-16 23:26:30 +0200

    Backup NixOS config 2024-10-16 23:26:30

--- backup.sh
+++ backup.sh
@@ -1,3 +1,17 @@
-git add .
-git commit -m "automatic configuration backup"
-git push -u origin main
+#git add .
+#git commit -m "automatic configuration backup"
+#git push -u origin main
+
+# Check if there are any changes
+if [[ $(git status --porcelain) ]]; then
+    # Stage all changes
+    git add .
+
+    # Commit the changes with a timestamp
+    git commit -m "Backup NixOS config $(date '+%Y-%m-%d %H:%M:%S')"
+
+    # Push the changes to GitHub
+    git push origin main
+else
+    echo "No changes to NixOS configuration."
+fi
