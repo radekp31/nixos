39c11a91abedaa5b351a3e97e6b2abe47277a0ae
Radek Polasek <polasek.31@seznam.cz>
2024-11-03 13:16:31 +0100

    update on 13:16

--- modules/usb/usb.nix
+++ modules/usb/usb.nix
@@ -6,8 +6,5 @@
 	services.devmon.enable = true;
 	services.gvfs.enable = true;
 	services.udisks2.enable = true;
-	services.udev.extraRules = ''
-     		ACTION=="add", SUBSYSTEMS=="usb", SUBSYSTEM=="block", ENV{ID_FS_USAGE}=="filesystem", RUN{program}+="${pkgs.systemd}/bin/systemd-mount --no-block --automount=yes --collect $devnode /media"       
-	'';
 }
 
