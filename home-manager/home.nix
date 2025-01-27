a69d97bc12bc1e3c0b173df7398e387284474b70
Radek Polasek <polasek.31@seznam.cz>
2025-01-27 17:01:29 +0100

    update on 2025-01-27 17:01:29

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -954,16 +954,78 @@ in
     enableZshIntegration = true;
     settings = {
       add_newline = false;
-      format = lib.concatStrings [
-        "$line_break"
-        "$package"
-        "$line_break"
-        "$character"
+      #format = lib.concatStrings [
+      #  "$line_break"
+      #  "$package"
+      #  "$line_break"
+      #  "$character"
+      #];
+      # format = lib.concatStrings [
+      #   #"$all"
+      #   """"
+      #   "$username\\"
+      #   "$hostname\\"
+      #   "$directory\\"
+      #   "$git_branch\\"
+      #   "$git_state\\"
+      #   "$git_status\\"
+      #   "$cmd_duration\\"
+      #   "$line_break\\"
+      #   "$python\\"
+      #   "$character"
+      #   """"
+      # ];
+      format = [
+        ""
+        "
+	$username\
+	$hostname\
+	$directory\
+	$git_branch\
+	$git_state\
+	$git_status\
+	$cmd_duration\
+	$line_break\
+	$python\
+	$character"
+        ""
       ];
       scan_timeout = 10;
       character = {
-        success_symbol = "➜";
-        error_symbol = "➜";
+        success_symbol = "[ ](bold green)";
+        error_symbol = "[ ](red)";
+        #vimcmd_symbol = "[❮](green)"
+      };
+      continuation_prompt = "[ ](bold green)";
+      directory = {
+        style = "purple";
+      };
+      git_branch = {
+        format = "[$branch]($style)";
+        style = "bright-black";
+      };
+      git_status = {
+        format = "[[(*$conflicted$untracked$modified$staged$renamed$deleted)](218) ($ahead_behind$stashed)]($style)";
+        style = "cyan";
+        conflicted = "​";
+        untracked = "​";
+        modified = "​";
+        staged = "​";
+        renamed = "​";
+        deleted = "​";
+        stashed = "≡";
+      };
+      git_state = {
+        format = "\\([$state( $progress_current/$progress_total)]($style)\\) ";
+        style = "bright-black";
+      };
+      cmd_duration = {
+        format = "[$duration]($style) ";
+        style = "yellow";
+      };
+      python = {
+        format = "[$virtualenv]($style) ";
+        style = "bright-black";
       };
     };
   };
