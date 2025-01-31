70f70db72cc0eca0a203068945687403e9a73c0e
Radek Polasek <polasek.31@seznam.cz>
2025-01-31 18:36:29 +0100

    update on 2025-01-31 18:36:29

--- configuration.nix
+++ configuration.nix
@@ -479,6 +479,10 @@
 
       nix-push-config = "nix fmt && git fetch --all && git add . && git commit -m \"update on $(date '+%Y-%m-%d %H:%M:%S')\" && git push";
 
+      lg1 = "git log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(auto)%d%C(reset)' --all";
+      lg2 = "git log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset)%C(auto)%d%C(reset)%n''          %C(white)%s%C(reset) %C(dim white)- %an%C(reset)'";
+      lg = "lg1";
+
     };
 
     ohMyZsh = {
