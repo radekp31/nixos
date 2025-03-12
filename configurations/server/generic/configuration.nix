b319825b44335b1851e0b24fe0ac0444e4a172c8
Radek Polasek <polasek.31@seznam.cz>
2025-03-12 23:22:13 +0100

    update on 2025-03-12 23:22:13

--- configurations/server/generic/configuration.nix
+++ configurations/server/generic/configuration.nix
@@ -1,9 +1,3 @@
-# Edit this configuration file to define what should be installed on
-# your system. Help is available in the configuration.nix(5) man page, on
-# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
-
-
-
 # flakes need to be enabled
 # nixpkgs nees to be added to channels
 # shell need to be forced by mkForce to bash
@@ -35,8 +29,6 @@
   boot.loader.grub.device = "/dev/sda"; # or "nodev" for efi only
 
   networking.hostName = "nixos-vps1"; # Define your hostname.
-  # Pick only one of the below networking options.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
   networking.networkmanager.enable = true; # Easiest to use and most distros use this by default.
 
   # Set nix flakes, and $NIX_PATH (hopefully this also fixed nix channels - update: It doesnt)
@@ -66,20 +58,26 @@
     enable = true;
     # Optional: Configure SSH settings
     settings = {
-      PasswordAuthentication = true; # Set to false to disable password login
+      PasswordAuthentication = false; # Set to false to disable password login
     };
+  };
+
+  services.nginx = {
+    enable = false;
   };
 
   # Open port 22 in the firewall
   networking.firewall = {
     enable = true; # Enable the firewall
-    allowedTCPPorts = [ 22 ]; # Allow SSH connections
+    allowedTCPPorts = [ 22 ]; # Allow SSH connections ; 80 is testing of remote deployment from localhost
     # Optional: if you're using IPv6
     # allowedTCPPorts = [ 22 ];       # Same for IPv6
+    extraCommands = ''
+    '';
   };
 
   # Set your time zone.
-  # time.timeZone = "Europe/Amsterdam";
+  time.timeZone = "Europe/Prague";
 
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
@@ -96,40 +94,7 @@
   # Enable the X11 windowing system.
   # services.xserver.enable = true;
 
-
-
-
-  # Configure keymap in X11
-  # services.xserver.xkb.layout = "us";
-  # services.xserver.xkb.options = "eurosign:e,caps:escape";
-
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
-
-  # Enable sound.
-  # services.pulseaudio.enable = true;
-  # OR
-  # services.pipewire = {
-  #   enable = true;
-  #   pulse.enable = true;
-  # };
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.libinput.enable = true;
-
   # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.radekp = {
-    isNormalUser = true;
-    extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
-    initialHashedPassword = "$y$j9T$qsPcS6PZU.ikacpJSivZw.$PcGv5mJTBRGSIQ1lzFUpzvPIb2cDpX3EDqWuvQQjOE2";
-    packages = with pkgs; [
-      tree
-      git
-      curl
-      vim
-    ];
-  };
-
   users.users.root = {
     isSystemUser = true;
     shell = lib.mkForce pkgs.bash;
@@ -160,56 +125,7 @@
     fuzzyCompletion = true;
   };
 
-  # programs.firefox.enable = true;
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  # environment.systemPackages = with pkgs; [
-  #   vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #   wget
-  # ];
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # Copy the NixOS configuration file and link it from the resulting system
-  # (/run/current-system/configuration.nix). This is useful in case you
-  # accidentally delete configuration.nix.
-  # system.copySystemConfiguration = true;
-
-  # This option defines the first version of NixOS you have installed on this particular machine,
-  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
-  #
-  # Most users should NEVER change this value after the initial install, for any reason,
-  # even if you've upgraded your system to a new NixOS release.
-  #
-  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
-  # so changing it will NOT upgrade your system - see https://nixos.org/manual/nixos/stable/#sec-upgrading for how
-  # to actually do that.
-  #
-  # This value being lower than the current NixOS release does NOT mean your system is
-  # out of date, out of support, or vulnerable.
-  #
-  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
-  # and migrated your data accordingly.
-  #
-  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
-  system.stateVersion = "25.05"; # Did you read the comment?
+  system.stateVersion = "25.05";
 
 }
