c2b1f6e7127cffb7298093c84ad112cb3d02471e
Radek Polasek <polasek.31@seznam.cz>
2025-11-13 22:49:34 +0000

    repo - nixpush test

--- flake.nix
+++ flake.nix
@@ -144,7 +144,7 @@
     #packages.${system}.alejandra = alejandra.packages.${system}.default;
     #
     #formatter.${system} = nixpkgs.legacyPackages.x86_64-linux.nixpkgs-fmt;
-
+    #
     # devShell = nixpkgs.mkShell {
     #   buildInputs = [ ... ];
     # };
--- work/nixos-wsl/home/radekp/home.nix
+++ work/nixos-wsl/home/radekp/home.nix
@@ -1,4 +1,4 @@
-{ ...}: {
+{...}: {
   imports = [
     ./packages.nix
     ../../../../modules/apps/shells/zsh/zsh.nix
