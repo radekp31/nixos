8e28c576a415a3ad98914305f5a0cf6b30ac2def
Radek Polasek <polasek.31@seznam.cz>
2025-11-14 14:11:30 +0000

    repo - fixing nixpush()

--- modules/apps/shells/zsh/zsh.nix
+++ modules/apps/shells/zsh/zsh.nix
@@ -9,24 +9,26 @@
       enable = true;
     };
     initContent = ''
-      export NIXPKGS_ALLOW_UNFREE=1
-      export EDITOR='nvim'
-      export VISUAL='nvim'
-      export C='/mnt/c'
+            export NIXPKGS_ALLOW_UNFREE=1
+            export EDITOR='nvim'
+            export VISUAL='nvim'
+            export C='/mnt/c'
 
-      #fastfetch --config ${pkgs.fastfetch}/share/fastfetch/presets/examples/3.jsonc
-      #nixpush() {
-      #  nix fmt && \
-      #  git add -A && \
-      #  git commit -m "$1" && \
-      #  nix flake check && \
-      #  git push
-      #}
+            #fastfetch --config ${pkgs.fastfetch}/share/fastfetch/presets/examples/3.jsonc
+            #nixpush() {
+            #  nix fmt && \
+            #  git add -A && \
+            #  git commit -m "$1" && \
+            #  nix flake check && \
+            #  git push
+            #}
 
       nixpush() {
         nix fmt && \
         git add -A && \
-        (git commit -m "$1" || true) && \
+        if ! git diff --cached --quiet; then
+          git commit -m "$1"
+        fi && \
         nix flake check && \
         git push
       }
