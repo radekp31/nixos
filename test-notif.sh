cd5952df758df43cd8f0a30db4193bf6641a35d3
Radek Polasek <polasek.31@seznam.cz>
2026-01-10 21:25:49 +0100

    cleanup

--- test-notif.sh
+++
@@ -1,7 +0,0 @@
-busctl --user call \
-  org.freedesktop.Notifications \
-  /org/freedesktop/Notifications \
-  org.freedesktop.Notifications \
-  Notify \
-  susssasa{sv}i \
-  -- "NixOS" 0 "nix-snowflake" "Working!" "No more unfinished commands." 0 0 -1
