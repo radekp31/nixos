440d767a8a4cdb9f4920bee3074d18e2a75b83aa
Radek Polasek <polasek.31@seznam.cz>
2026-01-17 19:51:54 +0100

    nixos-desktop - enabled catppucin for more packages

--- modules/home/colorschemes/catppuccin/default.nix
+++ modules/home/colorschemes/catppuccin/default.nix
@@ -2,20 +2,38 @@
   inputs,
   osConfig,
   ...
-}: {
+}: let
+  catppuccinMacchiato =
+    if (osConfig ? catppuccinTheme)
+    then osConfig.catppuccinTheme
+    else "macchiato";
+in {
   imports = [
     inputs.catppuccin.homeModules.catppuccin
   ];
 
   catppuccin = {
     enable = true;
-    flavor =
-      if (osConfig ? catppuccinTheme)
-      then osConfig.catppuccinTheme
-      else "macchiato";
+    flavor = catppuccinMacchiato;
 
     foot = {
       enable = true;
+      flavor = catppuccinMacchiato;
+    };
+
+    bat = {
+      enable = true;
+      flavor = catppuccinMacchiato;
+    };
+
+    btop = {
+      enable = true;
+      flavor = catppuccinMacchiato;
+    };
+
+    fzf = {
+      enable = true;
+      flavor = catppuccinMacchiato;
     };
   };
 }
