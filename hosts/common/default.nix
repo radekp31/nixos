0034f230a08cfed7c145081273b7cdeac32167ee
Radek Polasek <polasek.31@seznam.cz>
2026-03-10 00:28:07 +0100

    nixos-desktop - testing autoupdate on boot

--- hosts/common/default.nix
+++ hosts/common/default.nix
@@ -39,7 +39,8 @@ in {
   # Nix settings
   system.autoUpgrade = {
     enable = true; # too unstable
-    operation = "switch";
+    #operation = "switch";
+    operation = "boot";
     flake = "git+https://github.com/radekp31/nixos.git#${config.networking.hostName}";
     dates = "02:30";
     randomizedDelaySec = "30min";
