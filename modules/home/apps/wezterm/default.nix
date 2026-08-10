dfc572d03dfa3e8c62ce02abf09bb8699aa936a5
Radek Polasek <polasek.31@seznam.cz>
2026-08-10 14:02:54 +0000

    adding wezterm

--- modules/home/apps/wezterm/default.nix
+++ modules/home/apps/wezterm/default.nix
@@ -5,6 +5,9 @@
     #extraConfig = builtins.readFile ./wezterm.lua;
   };
 
-  # Symlink for Windows access (adjust target path as needed)
-  #xdg.configFile."wezterm/wezterm.lua".source = ./wezterm.lua;
+  # Symlink for Windows access (adjust target path as needed), Windows wont pick the config automatically
+  # Either: cp /mnt/c/Users/radek.polasek/.config/wezterm/wezterm.lua /etc/nixos/modules/home/apps/wezterm/wezterm.lua
+  #
+  # Or:  Copy-Item "\\wsl$\NixOS\etc\nixos\modules\home\apps\wezterm\wezterm_win.lua" -Destination "$env:USERPROFILE\.config\wezterm\wezterm.lua" -Force
+  
 }
